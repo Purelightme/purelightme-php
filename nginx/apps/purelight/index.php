5d95706076999280b247926e031fedf655c3067d
purelightme <purelightme@PurelightmeMBP.lan>
2020-06-28 18:20:57 +0800

    add cli service

--- nginx/apps/purelight/index.php
+++ nginx/apps/purelight/index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+//phpinfo();
+//exit;
+
+//$pdo = new PDO('mysql:host=mysql;dbname=mysql','root','root');
+//
+//var_dump($pdo);
+//
+//var_dump(gethostname());
+
+
+var_dump(file_get_contents("http://host.docker.internal/index.php"));
